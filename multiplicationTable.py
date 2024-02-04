b64eb906808809f2db5434c4fbeb56b194b7b99b
Diwakarthedeveloper <djha4635@gmail.com>
2024-02-04 14:37:22 +0530

    multiplication table using while loop

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,3 +1,9 @@
+# n = int(input("Enter the number whose table you need = "))
+# for i in range(1,11):
+#     print(f"{n} x {i} = ", n*i)
+
 n = int(input("Enter the number whose table you need = "))
-for i in range(1,11):
-    print(f"{n} x {i} = ", n*i)
+i = 1
+while i<=10:
+     print(f"{n} x {i} = ", n*i)
+     i=i+1
