0b6f2ce5a3c42eb2093588153edd25ff92359446
Diwakarthedeveloper <djha4635@gmail.com>
2024-02-04 15:28:35 +0530

    dynamic variable and range for multiplication table

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,9 +1,21 @@
 # n = int(input("Enter the number whose table you need = "))
-# for i in range(1,11):
-#     print(f"{n} x {i} = ", n*i)
+# i = 1
+# while i<=10:
+#      print(f"{n} x {i} = ", n*i)
+#      i=i+1
 
-n = int(input("Enter the number whose table you need = "))
-i = 1
-while i<=10:
-     print(f"{n} x {i} = ", n*i)
-     i=i+1
+while True:
+    n = int(input("Enter the number whose table you need = "))
+    r = int(input("Enter the range or end of the table you need like till 10 or 15 or 50 = "))
+    for i in range(1, r+1):
+        print(f"{n} x {i} = ", n*i)
+
+    repeat = input("Do you want next table then press y for YES or n for NO ->  ")
+    if repeat == "n":
+        break
+
+print("Thanks for using our multiplication application")
+
+
+
+
