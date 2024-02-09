c96ffc2d8e18cec45711c1c836a0d2404633f626
Diwakarthedeveloper <djha4635@gmail.com>
2024-02-10 01:45:42 +0530

    fixed size issues

--- digitalClock.py
+++ digitalClock.py
@@ -31,55 +31,55 @@ def date_time():
 
 clock = Tk()
 clock.title('  *****Digital Clock*****   ')
-clock.geometry('1000x500') # size of the app window
+clock.geometry('800x450') # size of the app window
 clock.config(bg='yellow')   # bg is redefined variable here bt pyrhon for background colour
 
 lab_hour=Label(clock,text="00",font=('Time New Roman',60,"bold"), bg = 'red',fg="white")
 lab_hour.place(x=40, y =40, height=110,width=100)
 
-lab_hour_txt = Label(clock,text="HOUR",font=('Time New Roman',20,"bold"), bg = 'red',fg="white")
+lab_hour_txt = Label(clock,text="HOUR",font=('Time New Roman',15,"bold"), bg = 'red',fg="white")
 lab_hour_txt.place(x=40, y =160, height=20,width=100)
 
 lab_min = Label(clock,text="00",font=('Time New Roman',60,"bold"), bg = 'red',fg="white")
 lab_min.place(x=200, y =40, height=110,width=100)
 
-lab_min_txt = Label(clock,text="MIN",font=('Time New Roman',20,"bold"), bg = 'red',fg="white")
+lab_min_txt = Label(clock,text="MIN",font=('Time New Roman',15,"bold"), bg = 'red',fg="white")
 lab_min_txt.place(x=200, y =160, height=20,width=100)
 
 lab_sec = Label(clock,text="00",font=('Time New Roman',60,"bold"), bg = 'red',fg="white")
 lab_sec.place(x=400, y =40, height=110,width=100)
 
-lab_sec_txt = Label(clock,text="SEC",font=('Time New Roman',20,"bold"), bg = 'red',fg="white")
+lab_sec_txt = Label(clock,text="SEC",font=('Time New Roman',15,"bold"), bg = 'red',fg="white")
 lab_sec_txt.place(x=400, y =160, height=20,width=100)
 
-lab_ampm = Label(clock,text="00",font=('Time New Roman',60,"bold"), bg = 'red',fg="white")
+lab_ampm = Label(clock,text="00",font=('Time New Roman',40,"bold"), bg = 'red',fg="white")
 lab_ampm.place(x=600, y =40, height=110,width=100)
 
-lab_ampm_txt = Label(clock,text="AM/PM",font=('Time New Roman',20,"bold"), bg = 'red',fg="white")
+lab_ampm_txt = Label(clock,text="AM/PM",font=('Time New Roman',15,"bold"), bg = 'red',fg="white")
 lab_ampm_txt.place(x=600, y =160, height=20,width=100)
 
 lab_date = Label(clock,text="00",font=('Time New Roman',60,"bold"), bg = 'red',fg="white")
 lab_date.place(x=40, y =200, height=110,width=100)
 
-lab_date_txt = Label(clock,text="DATE",font=('Time New Roman',20,"bold"), bg = 'red',fg="white")
+lab_date_txt = Label(clock,text="DATE",font=('Time New Roman',15,"bold"), bg = 'red',fg="white")
 lab_date_txt.place(x=40, y =350, height=20,width=100)
 
 lab_month = Label(clock,text="00",font=('Time New Roman',60,"bold"), bg = 'red',fg="white")
 lab_month.place(x=200, y =200, height=110,width=100)
 
-lab_month_txt = Label(clock,text="Month",font=('Time New Roman',20,"bold"), bg = 'red',fg="white")
+lab_month_txt = Label(clock,text="MONTH",font=('Time New Roman',15,"bold"), bg = 'red',fg="white")
 lab_month_txt.place(x=200, y =350, height=20,width=100)
 
 lab_year = Label(clock,text="00",font=('Time New Roman',60,"bold"), bg = 'red',fg="white")
 lab_year.place(x=400, y =200, height=110,width=100)
 
-lab_year_txt = Label(clock,text="YEAR",font=('Time New Roman',20,"bold"), bg = 'red',fg="white")
+lab_year_txt = Label(clock,text="YEAR",font=('Time New Roman',15,"bold"), bg = 'red',fg="white")
 lab_year_txt.place(x=400, y =350, height=20,width=100)
 
-lab_day = Label(clock,text="00",font=('Time New Roman',60,"bold"), bg = 'red',fg="white")
+lab_day = Label(clock,text="00",font=('Time New Roman',40,"bold"), bg = 'red',fg="white")
 lab_day.place(x=600, y =200, height=110,width=100)
 
-lab_day_txt = Label(clock,text="DAY",font=('Time New Roman',20,"bold"), bg = 'red',fg="white")
+lab_day_txt = Label(clock,text="DAY",font=('Time New Roman',15,"bold"), bg = 'red',fg="white")
 lab_day_txt.place(x=600, y =350, height=20,width=100)
 
 
