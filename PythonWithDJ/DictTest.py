68f00ee168947b6e2d87f4435a486c027c029852
Diwakar <djha4635@gmail.com>
2024-02-20 14:49:50 +0530

    Delete PythonWithDJ directory

--- PythonWithDJ/DictTest.py
+++
@@ -1,7 +0,0 @@
-#write a Dictionary program where the user input the name as key and get its value as answer.
-
-d2 = {"Diwakar":"Paratha","Guudu":"Soyachunk","Ram":"Roti"}
-
-user= input("whose name you want=")
-print(d2[user])
-
--- PythonWithDJ/WhileLoop.py
+++
@@ -1,5 +0,0 @@
-i = 0
-
-while(i<45):  # while loop keeps running till till the condition is true
-    print(i)
-    i = i +1
